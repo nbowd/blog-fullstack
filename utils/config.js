7ce1538d34ecc8f594f6fe41b133f450eec82ba4
nbowd <bowdenn@oregonstate.edu>
2021-07-05 16:02:54 -0700

    added jest for testing

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,9 @@
 
 require('dotenv').config()
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT
+// eslint-disable-next-line no-undef
 const MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = {
