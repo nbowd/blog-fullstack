1a439bf44b952565e08c466cd33393b2c102263c
nbowd <bowdenn@oregonstate.edu>
2021-07-05 21:04:41 -0700

    basic testing functionality added, uses supertest,  pre-async/await refactor

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,32 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let total = 0
+  for (const element of blogs) {
+    total += element.likes
+  }
+  return total
+}
+
+const favoriteBlog = (blogs) => {
+  if (!blogs) {return}
+  let title = blogs[0].title
+  let author = blogs[0].author
+  let likes = blogs[0].likes
+  for (const element of blogs) {
+    if (element.likes > likes) {
+      title = element.title
+      author = element.author
+      likes = element.likes
+    }
+  }
+  return {title, author, likes}
+  
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
