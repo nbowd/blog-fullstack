200489b85d38a6fd286613026a7ace8cef378db4
nbowd <bowdenn@oregonstate.edu>
2021-07-14 21:44:05 -0700

    blog details can expand/collapse on click

--- blog-frontend/src/components/Blog.js
+++ blog-frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  const [allDetails, setAllDetails] = useState(false)
+
+  const hideDetails = { display: allDetails ? 'none' : '' }
+  const showDetails = { display: allDetails ? '' : 'none' }
+
+  return <div style={blogStyle}>
+    <div style={hideDetails}>
+      {blog.title} {blog.author} <button onClick={() => setAllDetails(true)}>Details</button>
+    </div>  
+
+    <div style={showDetails}>
+      <div>{blog.title} <button onClick={() => setAllDetails(false)}>Hide</button> </div> 
+      <div>{blog.url}</div>
+      <div>{blog.likes} <button>Like</button> </div> 
+      <div>{blog.author}</div>
+    </div>
+  </div>
+}
 
 export default Blog
